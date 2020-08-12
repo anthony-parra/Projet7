a16d529ce36f8a7bef1d53140b32941d1e2d4a41
anthony-parra <anthonyparra62@gmail.com>
2020-08-12 13:01:32 +0200

    Connexion à la BDD MySQL et gestion des erreurs

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+
+
+// Connexion à la BDD MySQL
+console.log('Connexion en cours, veuillez patienter...');
+
+let connectionMysql = mysql.createConnection({
+    database: 'Groupomania',
+    host: "localhost",
+    port: '8889',
+    user: "root",
+    password: "root"
+});
+
+connectionMysql.connect(function(error) {
+    if (error)
+        console.log('Je crois que nous avons une erreur' + error)
+    else
+        console.log("Vous êtes maintenant connecté !");
+});
 
 
 const app = express();
