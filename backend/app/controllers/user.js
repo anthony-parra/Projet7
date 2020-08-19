1dd15ebb78312696bcfdf146509228cbdde79697
anthony-parra <anthonyparra62@gmail.com>
2020-08-19 11:18:20 +0200

    Installation de JWT et Bcrypt

--- backend/app/controllers/user.js
+++ backend/app/controllers/user.js
@@ -1,16 +1,19 @@
 const User = require("../models/user.js");
+const jsonWebToken = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-exports.create = (req, res) => {
+exports.signup = (req, res) => {
 
     if (!req.body) {
       res.status(400).send({
         message: "Le champ doit être rempli !"
       });
     }
-
-    const user = new User({
-      email: req.body.email,
-      password: req.body.password
+    bcrypt.hash(req.body.password, 10)
+      .then(hash => {
+        const user = new User({
+          email: req.body.email,
+          password: hash
     });
 
     User.create(user, (err, data) => {
@@ -21,6 +24,7 @@ exports.create = (req, res) => {
         });
       else res.send(data);
     });
+      })   
   };
 
   exports.delete = (req, res) => {
@@ -39,7 +43,7 @@ exports.create = (req, res) => {
     });
   };
 
-  exports.findOne = (req, res) => {
+  exports.login = (req, res) => {
     User.findById(req.params.userId, (err, data) => {
       if (err) {
         if (err.kind === "Non trouvé !") {
@@ -51,6 +55,23 @@ exports.create = (req, res) => {
             message: "On ne retrouve pas l'utilisateur avec l'id " + req.params.userId
           });
         }
-      } else res.send(data);
+      } else res.send(data)
+      bcrypt.compare(req.body.password, userId.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                    } else {
+                      res.status(200).json({
+                        userId: userId,
+                        token: jsonWebToken.sign({ userId: userId },
+                            'LA_CLE_SECRETE', { expiresIn: '24h' }
+                        )
+                    })
+                    return res.status(200).json({ message : 'Création du token réussi !'})
+                  }
+                })
+                .catch(error => res.status(500).json({ error }));
+                    
+      
     });
   };
--- backend/app/routes/user.js
+++ backend/app/routes/user.js
@@ -1,8 +1,7 @@
 module.exports = app => {
     const user = require("../controllers/user.js");
   
-    app.post("/user", user.create);
+    app.post("/user", user.signup);
+    app.get("/user/:userId", user.login);
     app.delete("/user/:userId", user.delete);
-    app.get("/user/:userId", user.findOne);
-  
   };
